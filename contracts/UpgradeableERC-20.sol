983ad054126bb84471883fb5c73ec3e155725f17
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2022-08-30 16:40:19 +0530

    updated erc20 contract

--- contracts/UpgradeableERC-20.sol
+++ contracts/UpgradeableERC-20.sol
@@ -2,13 +2,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract MoonToken is ERC20 {
-    address public admin;
-    constructor() ERC20("MoonToken", "MOON") {
-        _mint(msg.sender, 10000 * 10 ** 18);
-        admin = msg.sender;
+contract MoonTokenUpgradeable is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    
+
+    function initialize() external initializer {
+        __ERC20_init("MoonToken", "MOON");
+        __Ownable_init();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
     function burn(uint amount) external {
